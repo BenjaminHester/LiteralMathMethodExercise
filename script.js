52994c0cb8c72427feb5b74dab5963ffa0ea32cb
Benjamin Hester <benjamin.hester@stu.barren.kyschools.us>
2024-02-01 10:39:03 -0600

    Finished Literal/Math Method Exercise.

---
+++ script.js
@@ -0,0 +1,11 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs."
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like","love"));
+let beenImpaled = "Oh, look at that. I've been impaled."
+console.log(beenImpaled.slice(18,36));
+let dotDotDot = "..."
+let skullBones = `I don't have a skull${dotDotDot}or bones.`
+console.log(Math.PI);
+let randomNumber = Math.random()*3 +1;
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
